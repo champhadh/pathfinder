2a17746ef1bf0d49965e47fcc1cdc4899576edd6
Hady Wehbe <110308285+champhadh@users.noreply.github.com>
2025-02-03 12:06:25 -0500

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,4 @@
-import Grid from "@/components/Grid";
-
-
-export default function Home() {
-  return (
-    <div className="flex h-screen items-center justify-center bg-gray-100">
-      <button className="px-6 py-3 text-white bg-blue-500 rounded-lg hover:bg-blue-700">
-        Test Button
-      </button>
-    </div>
-  );
-}
+import Grid from "../components/Grid";
 
 
 
